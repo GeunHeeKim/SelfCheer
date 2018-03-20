3a5385828369c1f90ae46b5e6495c53151ac96e7
Geun Hee Kim <geunhee0219@gmail.com>
2018-03-20 17:57:12 +0900

    feat(add delete)vue file add

---
+++ src/components/Delete.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    delete page
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Delete from '@/components/Delete'
+import Add from '@/components/Add'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/delete',
+      name: 'Delete',
+      component: Delete
+    },
+    {
+      path: '/add',
+      name: 'Add',
+      component: Add
     }
   ]
 })
